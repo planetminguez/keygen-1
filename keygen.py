4b51fc1dadc65d654c469a136b7f4160cfeba62e
Exceen <d11001@me.com>
2012-11-14 19:01:10 +0100

    improved code

--- keygen.py
+++ keygen.py
@@ -8,34 +8,46 @@ from os import system
 from os.path import exists
 from random import randint
 
+try:
+	script, filename = argv
+except:
+	filename = False
+
 class keygen(object):
 
 	def __init__(self):
-
 		self.letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') # you can add some letters, but leave A-Z and a-z there!
 		self.letterslength = len(self.letters)
 		self.tmpkey = self.letters[randint(0, 52)]
 		
-		# old code:
-		# a = ord('A')
-		# z = ord('Z')
-		# for i in xrange(a, z+1):
-		# letters.append(str(unichr(i)))
-		# a = ord(str(unichr(a)).lower()) #97
-		# z = ord(str(unichr(z)).lower()) #122
-		# for i in xrange(a, z+1):
-		# 	letters.append(str(unichr(i)))
-		# for i in xrange(0, 10):
-		# 	letters.append(i)'
+	def create_keys(self):
+		key_info = keygen_information()
+		ex_file = external_file()
 
-	def create_tmpkey(self):
+		for i in xrange(0, key_info.amount):
+			
+			keys = keygen() # first character already initialized with a letter
+			keys.create_tmpkey(key_info.length)
+	
+			print keys.tmpkey
+	
+			if filename:
+				keys.tmpkey = '%s\n' % keys.tmpkey
+				ex_file.dataset.write(keys.tmpkey)
+	
+		ex_file.close_file()
+
+	def create_tmpkey(self, length_of_a_key):
 		# creating a temporary key
-		self.tmpkey = '%s%s' % (self.tmpkey, self.letters[randint(0, self.letterslength-1)])
+		for i in xrange(0, length_of_a_key - 1):
+			self.tmpkey = '%s%s' % (self.tmpkey, self.letters[randint(0, self.letterslength-1)])
 
-class questions(keygen):
+class keygen_information(keygen):
 
 	def __init__(self):
 		self.prompt = '> '
+		self.amount = self.amount_of_keys()
+		self.length = self.length_of_a_single_key()
 
 	def amount_of_keys(self):
 		print 'How many keys do you want to create?'
@@ -46,8 +58,7 @@ class questions(keygen):
 		return int(raw_input(self.prompt))
 
 class external_file(keygen):
-	
-	def __init__(self, filename):
+	def __init__(self):
 
 		if not filename:
 			print 'If you want to save the file with the keys, you have\nto add a second argument with the name of the file.'
@@ -58,48 +69,15 @@ class external_file(keygen):
 		else:
 			print 'The file %r doesn\'t exist, it will be created' % filename
 
-	def open_file(self, filename):
 		if filename:
 			self.dataset = open(filename, 'w')
 
-	def close_file(self, filename):
+	def close_file(self):
 		if filename is not False:
 			self.dataset.close()
 
-
 def main():
-	try:
-		script, filename = argv
-	except:
-		filename = False
-	
-	key_info = questions()
-
-	amount = key_info.amount_of_keys()
-	length_of_a_key = key_info.length_of_a_single_key()
-
-	ex_file = external_file(filename)
-	ex_file.open_file(filename)
-	
-	###############################
-	# creating, printing and writing the key(s)
-
-	for i in xrange(0, amount):
-		
-		keys = keygen() # first character already initialized with a letter
-
-		for i in xrange(0, length_of_a_key - 1):
-			keys.create_tmpkey()
-
-		print keys.tmpkey
-
-		if filename:
-			keys.tmpkey = '%s\n' % keys.tmpkey
-			ex_file.dataset.write(keys.tmpkey)
-	
-	ex_file.close_file(filename)
-
-
+	keygen().create_keys()
 
 if __name__ == '__main__':
 	main()
